acbecd4d274abb55edd04bb6551ca506406ce4e3
Faster190 <113011702+Faster190@users.noreply.github.com>
2024-06-02 13:44:55 +0300

    Create module_4_1.py

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+
+print(fake_divide(4, 2))
+print(fake_divide(4, 0))
+print(true_divide(9, 3))
+print(true_divide(9, 0))
