30cd96f3152f474048042f122070096038b27a03
Faster190 <113011702+Faster190@users.noreply.github.com>
2024-07-31 15:06:13 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -13,44 +13,40 @@ class Shop:
 
     def get_products(self):
         file = open(self.__file_name, 'a')
+        file.close()
         file = open(self.__file_name, 'r')
         products = file.read()
         file.close()
         return products
 
     def add(self, *products):
-        string = self.get_products()  # Строка с информацией из файла
         list_ = []  # Список для названий продуктов из файла
-        word = ""  # Временное хранилище имени продукта
-        comma = True  # Указатель на участок строки с именем продукта
-        for i in range(len(string)):  # Заносим в отдельный список названия продуктов из файла
-            if string[i] == ',':
-                if comma:
-                    list_.append(word)
-                word = ""
-                comma = False
-            if comma:
-                word += string[i]
-            if string[i] == '\n':
-                comma = True
-        file = open(self.__file_name, 'a')
-        for i in range(len(products)):
-            verify = True
-            for j in range(i - 1):
-                if products[i].name == products[j].name:
-                    verify = False
-                    print(f"Продукт {products[i].name} уже есть в магазине")
+        new_product = True
+        for j in range(len(products)):
+            if new_product:
+                string = self.get_products()  # Строка с информацией из файла
+                word = ""  # Временное хранилище имени продукта
+                comma = True  # Указатель на участок строки с именем продукта
+                for i in range(len(string)):  # Заносим в отдельный список названия продуктов из файла
+                    if string[i] == ',':
+                        if comma:
+                            list_.append(word)
+                        word = ""
+                        comma = False
+                    if comma:
+                        word += string[i]
+                    if string[i] == '\n':
+                        comma = True
+            new_product = True
+            for i in range(len(list_)):
+                if products[j].name == list_[i]:
+                    new_product = False
+                    print(f"Продукт {products[j].name} уже есть в магазине")
                     break
-            if not verify:
-                continue
-            for j in range(len(list_)):
-                if products[i].name == list_[j]:
-                    verify = False
-                    print(f"Продукт {products[i].name} уже есть в магазине")
-                    break
-            if verify:
-                file.write(f"{str(products[i])}\n")
-        file.close()
+            if new_product:
+                file = open(self.__file_name, 'a')
+                file.write(f"{str(products[j])}\n")
+                file.close()
 
 
 s1 = Shop()
