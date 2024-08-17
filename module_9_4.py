49ea82903e985e9405831fa744dcb440905e6e2e
Faster190 <113011702+Faster190@users.noreply.github.com>
2024-08-18 02:56:31 +0300

    Update module_9_4.py

--- module_9_4.py
+++ module_9_4.py
@@ -8,7 +8,7 @@ print(list(map(compare, first, second)))
 
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
-        file = open(file_name, 'a', encoding="utf-8")
+        file = open(file_name, 'w', encoding="utf-8")
         for i in data_set:
             file.write(f"{str(i)}\n")
         file.close()
