248792f55afe11033d2a9b4656c2387b626b6422
Faster190 <113011702+Faster190@users.noreply.github.com>
2024-09-10 05:13:05 +0300

    Create module_14_1.py

---
+++ module_14_1.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"User{i}", f"example{i}@gmail.com", i*10, 1000))
+for i in range(1, 11, 2):
+    cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
+for i in range(1, 11, 3):
+    cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+result = cursor.fetchall()
+for i in result:
+    print(f"Имя: {i[0]} | Почта: {i[1]} | Возраст: {i[2]} | Баланс: {i[3]}")
+connection.commit()
+connection.close()
