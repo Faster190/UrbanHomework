d6bbc431b2afd37ed1bc6898f443f58b5459478a
Faster190 <113011702+Faster190@users.noreply.github.com>
2024-09-19 19:49:28 +0300

    Create module_16_1.py

---
+++ module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def welcome():
+    return "Главная страница"
+
+
+@app.get('/user/admin')
+async def admin():
+    return "Вы вошли как администратор"
+
+
+@app.get('/user/{user_id}')
+async def admin(user_id: int):
+    return f"Вы вошли как пользователь № {user_id}"
+
+
+@app.get('/user')
+async def admin(username: str, age: int):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
